f38bbd411eaa5d2d6886fbacc6f4b03898d2fcb2
wesley <wesleys.c190@gmail.com>
2026-03-22 00:03:05 -0300

    ajustes na navbar

---
+++ assets/home_page/script.js
@@ -0,0 +1,32 @@
+const slides = document.querySelectorAll(".slide")
+
+let index = 0
+
+function showSlide(){
+
+slides.forEach(slide =>{
+slide.classList.remove("active")
+})
+
+slides[index].classList.add("active")
+
+}
+
+setInterval(()=>{
+
+index++
+
+if(index >= slides.length){
+index = 0
+}
+
+showSlide()
+
+},3000)
+
+const menuToggle = document.querySelector(".menu-toggle");
+const sideMenu = document.getElementById("sideMenu");
+
+menuToggle.addEventListener("click", () => {
+    sideMenu.classList.toggle("active");
+});
